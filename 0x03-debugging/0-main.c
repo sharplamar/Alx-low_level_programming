b30a537d62dbf248582247270a3b13f1a7c91ccc
sharplamar <vmwangi008@gmail.com>
2022-09-17 19:51:49 -0700

    esyt

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+	if (i / 2 == 0)
+		printf("positive\n");
+	else
+		print("negative\n")
+        return (0);
+}
